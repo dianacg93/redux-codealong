a5b92de8329063e931039864592f467be0c05387
DianaG <diana.cg93@gmail.com>
2021-02-03 19:01:52 -0500

    setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
 import './index.css';
 import App from './App';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import rootReducer from './reducers';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+//Redux thunk -> Async middleware for Redux
+
+//Redux store:
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+// the create store is taking two arguments: a reducer and an enhacer
+// enhancer is used to specify it to enhance the store
+// with third party capabilities such as middleware, time travel, persistence
+// The only store enhancer that ships with redux is applyMiddleware()
+//The reason we need thunk is because asynchronous code can't be passed to reducers or the dispatch method so we use thunk to perform async side effects before calling dispatch
+render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
